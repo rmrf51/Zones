8493b116736161c4e071f1d17bc4fd11244c4f76
rmrf51 <donat0009@outlook.com>
2020-03-31 13:26:40 +0300

    Zone

---
+++ Zone/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Zone {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Zones z = new Zones();
+            z.SumZones(2, 2);
+        }
+    }
+}
---
+++ Zone/Zones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Zone {
+    public class Zones {
+        public int SumZones(int a, int b) {
+            int c = a + b;
+            File.Create("./file.txt");
+            File.AppendAllText("./file.txt", c.ToString());
+            return c;
+        }
+    }
+}
